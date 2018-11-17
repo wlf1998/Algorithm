e06d220cf3852d6a4d980c592a67aaaa3e32c8d4
Lifeng Wang <wlf1998@mail.ustc.edu.cn>
2018-11-18 00:15:44 +0800

    RBTree-Interval 4.0

--- RBTree/RBTReeInterval.cpp
+++ RBTree/RBTReeInterval.cpp
@@ -10,273 +10,277 @@ struct Node{
     int max;
     string color;
     Node *pParent;
-    Node *pLeft;
-    Node *pRight;
+    Node *left;
+    Node *right;
 };
 
 class RBT{
     public:
         RBT();
         ~RBT();
-        void LeftRotate(Node *px);
-        void RightRotate(Node *px);
-        void Insert(Node *pz);
-        void InsertFixUp(Node *pz);
-        void Transplant(Node *px,Node *py);
-        void Delete(Node *pz);
-        void DeleteFixUp(Node *pz);
-        void PreorderTreeWalk(Node *pz);
-        void InorderTreeWalk(Node *pz);
-        Node *TreeMinimum(Node *pz);
-        Node *GetRoot(){return this->pT_root;}
-        Node *GetNil(){return this->pT_nil;}
+        void leftRotate(Node *x);
+        void RightRotate(Node *x);
+        void Insert(Node *z);
+        void InsertFixUp(Node *z);
+        void Transplant(Node *x,Node *y);
+        void Delete(Node *z);
+        void DeleteFixUp(Node *z);
+        void PreorderTreeWalk(Node *z);
+        void InorderTreeWalk(Node *z);
+        Node *TreeMinimum(Node *z);
+        Node *GetRoot(){return this->root;}
+        Node *GetNil(){return this->nil;}
         Node *IntervalSearch(Node* i);
     private:
-        Node *pT_nil;
-        Node *pT_root;
+        Node *nil;
+        Node *root;
 };
 
 RBT::RBT(){
-    pT_nil = new Node;
-    pT_nil->color = "Black";
-    pT_nil->max = 0;
-    pT_root = pT_nil;
+    nil = new Node;
+    nil->color = "Black";
+    nil->max = 0;
+    root = nil;
 }
 
 RBT::~RBT(){
-    if(pT_nil != NULL)
-        delete pT_nil;
+    if(nil != NULL)
+        delete nil;
 }
 
-Node *RBT::TreeMinimum(Node *pz){
-    while(pz->pLeft != pT_nil)
-        pz = pz->pLeft;
-    return pz;
+Node *RBT::TreeMinimum(Node *z){
+    while(z->left != nil)
+        z = z->left;
+    return z;
 }
-void RBT::LeftRotate(Node *px){
-    Node *py = px->pRight;
-    px->pRight = py->pLeft;
-    if(py->pLeft != pT_nil)
-        py->pLeft->pParent = px;
-    py->pParent = px->pParent;
-    if(px->pParent == pT_nil)
-        pT_root == py;
-    else if(px==px->pParent->pLeft)
-        px->pParent->pLeft = py;
+void RBT::leftRotate(Node *x){
+    Node *y = x->right;
+    x->right = y->left;
+    if(y->left != nil)
+        y->left->pParent = x;
+    y->pParent = x->pParent;
+    if(x->pParent == nil)
+        root == y;
+    else if(x==x->pParent->left)
+        x->pParent->left = y;
     else 
-        px->pParent->pRight = py;
-    py->pLeft = px;
-    px->pParent = py;
-    py->max = px->max;
-    px->max = max(px->max, max(px->pLeft->max, px->pRight->max));
+        x->pParent->right = y;
+    y->left = x;
+    x->pParent = y;
+    y->max = x->max;
+    x->max = max(x->max, max(x->left->max, x->right->max));
 }
 
-void RBT::RightRotate(Node *py){
-    Node *px = py->pLeft;
-    py->pLeft = px->pRight;
-    px->pRight->pParent = py;
-    px->pParent = py->pParent;
-    if(py->pParent == pT_nil)
-        pT_root == px;
-    else if(py == py->pParent->pLeft)
-        py->pParent->pLeft = px;
+void RBT::RightRotate(Node *y){
+    Node *x = y->left;
+    y->left = x->right;
+    x->right->pParent = y;
+    x->pParent = y->pParent;
+    if(y->pParent == nil)
+        root == x;
+    else if(y == y->pParent->left)
+        y->pParent->left = x;
     else 
-        py->pParent->pRight = px;
-    px->pRight = py;
-    py->pParent = px;
-    px->max = py->max;
-    py->max = max(py->max, max(py->pLeft->max,py->pRight->max));
+        y->pParent->right = x;
+    x->right = y;
+    y->pParent = x;
+    x->max = y->max;
+    y->max = max(y->max, max(y->left->max,y->right->max));
 }
 
-void RBT::Insert(Node *pz){
-    pz->max = pz->high;
-    Node *py = pT_nil;
-    Node *px = pT_root;
-    while(px != pT_nil){
-        px->max = max(pz->high, px->max);
-        py = px;
-        if(pz->low < px->low)
-            px = px->pLeft;
+void RBT::Insert(Node *z){
+    z->max = z->high;
+    Node *y = nil;
+    Node *x = root;
+    while(x != nil){
+        x->max = max(z->high, x->max);
+        y = x;
+        if(z->low < x->low)
+            x = x->left;
         else
-            px = px->pRight;
+            x = x->right;
     }
-    pz->pParent = py;
-    if(py == pT_nil)
-        pT_root = pz;
-    else if(pz->low < py->low)
-        py->pLeft = pz;
+    z->pParent = y;
+    if(y == nil)
+        root = z;
+    else if(z->low < y->low)
+        y->left = z;
     else 
-        py->pRight = pz;
-    pz->pLeft = pT_nil;
-    pz->pRight = pT_nil;
-    pz->color = "Red";
-    InsertFixUp(pz);
+        y->right = z;
+    z->left = nil;
+    z->right = nil;
+    z->color = "Red";
+    InsertFixUp(z);
 }
 
-void RBT::InsertFixUp(Node *pz){
-    Node *py = NULL;
-    while("Red" == pz->pParent->color){
-        if(pz->pParent == pz->pParent->pParent->pLeft){
-            py =  pz->pParent->pParent->pRight;
-            if(py->color == "Red"){
-                pz->pParent->color = "Black";
-                py->color = "Black";
-                pz->pParent->pParent->color = "Red";
-                pz = pz->pParent->pParent;
+void RBT::InsertFixUp(Node *z){
+    Node *y = NULL;
+    while("Red" == z->pParent->color){
+        if(z->pParent == z->pParent->pParent->left){
+            y =  z->pParent->pParent->right;
+            if(y->color == "Red"){
+                z->pParent->color = "Black";
+                y->color = "Black";
+                z->pParent->pParent->color = "Red";
+                z = z->pParent->pParent;
             }
             else{
-                if(pz == pz->pParent->pRight){
-                    pz = pz->pParent;
-                    LeftRotate(pz);
+                if(z == z->pParent->right){
+                    z = z->pParent;
+                    leftRotate(z);
                 }
-                pz->pParent->color = "Black";
-                pz->pParent->pParent->color = "Red";
-                RightRotate(pz->pParent->pParent);
+                z->pParent->color = "Black";
+                z->pParent->pParent->color = "Red";
+                RightRotate(z->pParent->pParent);
             }
         }
-        else if(pz->pParent == pz->pParent->pParent->pRight){
-            py = pz->pParent->pParent->pLeft;
-            if(py->color == "Red"){
-                pz->pParent->color = "Black";
-                py->color = "Black";
-                pz->pParent->pParent->color = "Red";
-                pz = pz->pParent->pParent;
+        else if(z->pParent == z->pParent->pParent->right){
+            y = z->pParent->pParent->left;
+            if(y->color == "Red"){
+                z->pParent->color = "Black";
+                y->color = "Black";
+                z->pParent->pParent->color = "Red";
+                z = z->pParent->pParent;
             }
             else{
-                if(pz ==pz->pParent->pLeft){
-                    pz = pz->pParent;
-                    RightRotate(pz);
+                if(z ==z->pParent->left){
+                    z = z->pParent;
+                    RightRotate(z);
                 }
-                pz->pParent->color = "Black";
-                pz->pParent->pParent->color = "Red";
-                LeftRotate(pz->pParent->pParent);
+                z->pParent->color = "Black";
+                z->pParent->pParent->color = "Red";
+                leftRotate(z->pParent->pParent);
             }
         }
     }
-    pT_root->color = "Black";
+    root->color = "Black";
 }
 
 void RBT::Transplant(Node *u, Node *v){
-    if(u->pParent == pT_nil)
-        pT_root = v;
-    else if(u == u->pParent->pLeft)
-        u->pParent->pLeft = v;
+    if(u->pParent == nil)
+        root = v;
+    else if(u == u->pParent->left)
+        u->pParent->left = v;
     else 
-        u->pParent->pRight = v;
+        u->pParent->right = v;
     v->pParent = u->pParent;
 }
 
-void RBT::Delete(Node *pz){
-    Node *py = pT_nil;
-    Node *px = pT_root;
-    py = pz;
-//    string y_original_color;
-//    y_original_color = py->color;
-    if(pz->pLeft == pT_nil){
-        px = pz->pRight;
-        Transplant(pz,pz->pRight);
+void RBT::Delete(Node *z){
+    Node *y = nil;
+    Node *x = root;
+    y = z;
+    string y_original_color;
+    y_original_color = y->color;
+    if(z->left == nil){
+        x = z->right;
+        Transplant(z,z->right);
     }
-    else if(pz->pRight == pT_nil){
-        px = pz->pLeft;
-        Transplant(pz,pz->pLeft);
+    else if(z->right == nil){
+        x = z->left;
+        Transplant(z,z->left);
     }
     else{
-        py = TreeMinimum(pz->pRight);
-//        y_original_color = py->color;
-        px = py->pRight;
-        if(py->pParent == pz)
-            px->pParent = py;
+        y = TreeMinimum(z->right);
+        y_original_color = y->color;
+        x = y->right;
+        if(y->pParent == z)
+            x->pParent = y;
         else{
-            Transplant(py,py->pRight);
-            py->pRight = pz->pRight;
-            py->pRight->pParent = py;
+            Transplant(y,y->right);
+            y->right = z->right;
+            y->right->pParent = y;
         }
-        Transplant(pz,py);
-        py->pLeft = pz->pLeft;
-        py->pLeft->pParent = py;
-        py->color = pz->color;
+        Transplant(z,y);
+        y->left = z->left;
+        y->left->pParent = y;
+        y->color = z->color;
     }
-    if(py->color == "BLACK")  
-        DeleteFixUp(px);
+    if(y->color == "Black")  
+        DeleteFixUp(x);
 }
 
-void RBT::DeleteFixUp(Node *px){
+void RBT::DeleteFixUp(Node *x){
     Node *pw = NULL;
-    while(px != pT_root && px->color == "BLACK"){
-        if(px == px->pParent->pLeft){
-            pw = px->pParent->pRight;
-            if(pw->color == "RED"){
-                pw->color == "BLACK";
-                px->pParent->color == "RED";
-                LeftRotate(px->pParent);
-                pw = px->pParent->pRight;
+    while(x != root && x->color == "Black"){
+        if(x == x->pParent->left){
+            pw = x->pParent->right;
+            if(pw->color == "Red"){
+                pw->color == "Black";
+                x->pParent->color == "Red";
+                leftRotate(x->pParent);
+                pw = x->pParent->right;
             }
-            if(pw->pLeft->color == "BLACK" && pw->pRight->color == "BLACK"){
-                pw->color = "RED";
-                px = px->pParent;
+            if(pw->left->color == "Black" && pw->right->color == "Black"){
+                pw->color = "Red";
+                x = x->pParent;
             }
-            else if(pw->pRight->color == "BLACK"){
-                pw->pLeft->color = "BLACK";
-                pw->color = "RED";
-                RightRotate(pw);
-                pw = px->pParent->pRight;
+            else{
+                if(pw->right->color == "Black"){
+                    pw->left->color = "Black";
+                    pw->color = "Red";
+                    RightRotate(pw);
+                    pw = x->pParent->right;
+                }
+                pw->color = x->pParent->color;
+                x->pParent->color = "Black";
+                pw->right->color = "Black";
+                leftRotate(x->pParent);
+                x = root;
             }
-            pw->color = px->pParent->color;
-            px->pParent->color = "BLACK";
-            pw->pRight->color = "BLACK";
-            LeftRotate(px->pParent);
-            px = pT_root;
         }
-        else if(px == px->pParent->pRight){
-            pw = px->pParent->pLeft;
-            if(pw->color == "RED"){
-                pw->color == "BLACK";
-                px->pParent->color == "RED";
-                RightRotate(px->pParent);
-                pw = px->pParent->pLeft;
+        else if(x == x->pParent->right){
+            pw = x->pParent->left;
+            if(pw->color == "Red"){
+                pw->color == "Black";
+                x->pParent->color == "Red";
+                RightRotate(x->pParent);
+                pw = x->pParent->left;
             }
-            if(pw->pRight->color == "BLACK" && pw->pLeft->color == "BLACK"){
-                pw->color = "RED";
-                px = px->pParent;
+            if(pw->right->color == "Black" && pw->left->color == "Black"){
+                pw->color = "Red";
+                x = x->pParent;
             }
-            else if(pw->pLeft->color == "BLACK"){
-                pw->pRight->color = "BLACK";
-                pw->color = "RED";
-                LeftRotate(pw);
-                pw = px->pParent->pLeft;
+            else{ 
+                if(pw->left->color == "Black"){
+                    pw->right->color = "Black";
+                    pw->color = "Red";
+                    leftRotate(pw);
+                    pw = x->pParent->left;
+                }
+                pw->color = x->pParent->color;
+                x->pParent->color = "Black";
+                pw->left->color = "Black";
+                RightRotate(x->pParent);
+                x = root;
             }
-            pw->color = px->pParent->color;
-            px->pParent->color = "BLACK";
-            pw->pLeft->color = "BLACK";
-            RightRotate(px->pParent);
-            px = pT_root;
         }
     }
-    px->color = "BLACK";
+    x->color = "Black";
 }
 
-void RBT::PreorderTreeWalk(Node *px){
-    if(px != pT_nil){
-        cout << "low: " <<px->low << "	high:" << px->high << "	max: " << px->max << "	color: " << px->color << endl;
-        PreorderTreeWalk(px->pLeft);
-        PreorderTreeWalk(px->pRight);
+void RBT::PreorderTreeWalk(Node *x){
+    if(x != nil){
+        cout << "low: " <<x->low << "	high:" << x->high << "	max: " << x->max << "	color: " << x->color << endl;
+        PreorderTreeWalk(x->left);
+        PreorderTreeWalk(x->right);
     }
 }
-void RBT::InorderTreeWalk(Node *px){
-    if(px != pT_nil){
-        InorderTreeWalk(px->pLeft);
-        cout << "low: " <<px->low << "	high:" << px->high << "	max: " << px->max << "	color: " << px->color << endl;
-        InorderTreeWalk(px->pRight);
+void RBT::InorderTreeWalk(Node *x){
+    if(x != nil){
+        InorderTreeWalk(x->left);
+        cout << "low: " << x->low << "	high:" << x->high << "	max: " << x->max << "	color: " << x->color << endl;
+        InorderTreeWalk(x->right);
     }
 }
 
 Node *RBT::IntervalSearch(Node *i){
-    Node *x = pT_root;
-    while(x != pT_nil && (x->high < i->low || i->high < x->low)){
-        if(x->pLeft != pT_nil && x->pLeft->max >= i->low)
-            x = x->pLeft;
+    Node *x = root;
+    while(x != nil && (x->high < i->low || i->high < x->low)){
+        if(x->left != nil && x->left->max >= i->low)
+            x = x->left;
         else
-            x = x->pRight;
+            x = x->right;
     }
     return x;
 }
@@ -313,21 +317,24 @@ int main(void){
         cin >> temp.low >> temp.high;
         Node *p =rbt.IntervalSearch(&temp);
         if(p != rbt.GetNil())
-            cout << p->low << '-' << p->color << '-' << p->max << '.' << endl;
+            cout << "low: " << p->low << "	high:" << p->high << "	max: " << p->max << "	color: " << p->color << endl;
         else
             cout << "No Overlap Interval" << endl;
-        cout << "continue? 1 for yes or 0 for no: ";
+        cout << endl << "Input delete: ";
+        int i;
+        cin >> i;
+        rbt.Delete(&ptemp[i]);
+        cout << endl;
+        cout << "InorderTreeWalk" << endl;
+        rbt.InorderTreeWalk(rbt.GetRoot());
+        cout << endl;
+        cout << "PreorderTreeWalk" << endl;
+        rbt.PreorderTreeWalk(rbt.GetRoot());
+        cout << endl;
+
+        cout << "1 to continue or 0 to exit (1/0):";
         cin >> bquit;
     }
-    
-    rbt.Delete(&ptemp[5]);
-    cout << "InorderTreeWalk" << endl;
-    rbt.InorderTreeWalk(rbt.GetRoot());
-    cout << endl;
-    cout << "PreorderTreeWalk" << endl;
-    rbt.PreorderTreeWalk(rbt.GetRoot());
-    cout << endl;
-    getchar();
     delete []ptemp;
     system("pause");
     return 0;
