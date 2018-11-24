fc380c0f6482fe76157613db8395fe7aef177c58
Lifeng Wang <wlf1998@mail.ustc.edu.cn>
2018-11-24 22:40:24 +0800

    LCS2.0

--- LCS/LCS.cpp
+++ LCS/LCS.cpp
@@ -1,3 +1,8 @@
+/***************
+ * Longest Common Subsequence Problem
+ * Lifeng Wang
+ * 2018.11.24
+ * *************/
 #include <vector>
 #include <string>
 #include <iostream>
@@ -5,15 +10,15 @@
 #include <sstream>
 using namespace std;
 
-//输出最长公共子串（LCS）
-//二维数组veca记录的是两个字符串Xi和Yj的LCS长度
-int LCS_length(const string &str1, const string &str2, 
-	            vector<vector<int> > &veca, vector<vector<int> > &vecb) {
+/*********
+ * output lcs
+ * veca is the length of Xi and Yj
+ * *******/ 
+int LCS_length(const string &str1, const string &str2, vector< vector<int> > &veca, vector< vector<int> > &vecb) {
 	int i, j;
 	int biggest = 0;
 	if (str1 == "" || str2 == "")
 		return 0;
-
 	for (i = 0; i <= str1.length(); i++) {
 		veca[i][0] = 0;
 	}
@@ -22,8 +27,8 @@ int LCS_length(const string &str1, const string &str2,
 	}
 	for (i = 1; i <= str1.length(); i++) {
 		for (j = 1; j <= str2.length(); j++) {
-			//如果Xi-1 == Yj-1，那么最长子序列为veca[i - 1][j - 1] + 1
-			//此时将vecb[i][j] = 1表明str1[i-1]是子问题LCS的一个元素
+			//if Xi-1 == Yj-1, then lcs is veca[i-1][j-1]+1
+			//at this time, veca[i][j] = 1 shows that str[i-1] is an element of child LCS
 			if (str1[i - 1] == str2[j - 1]) {
 				veca[i][j] = veca[i - 1][j - 1] + 1;
 				vecb[i][j] = 1;
@@ -43,8 +48,10 @@ int LCS_length(const string &str1, const string &str2,
 	return veca[str1.length()][str2.length()];
 }
 
-//该函数用于输出一个LCS的序列
-//这里输出的顺序是先向上寻找，再向左寻找
+/**************
+ * print an LCS order
+ * the order is up first, then left
+ * ***********/ 
 void PrintOneLCS(vector<vector<int> > &vecb, string &str1, int i, int j) {
 	if (i == 0 || j == 0)
 		return;
@@ -58,6 +65,11 @@ void PrintOneLCS(vector<vector<int> > &vecb, string &str1, int i, int j) {
 		PrintOneLCS(vecb, str1, i, j - 1);
 }
 
+/***************
+ * init veca to a two-dimensional array, line = str1.length + 1, raw = str2.length +1 
+ * veca is the length of Xi and Yj
+ * vecb is the best solution of veca[i][j]'s child problem
+ * *************/
 int main() {
 	string input;
 	getline(cin, input);
@@ -65,11 +77,8 @@ int main() {
 	string str1, str2;
 	ss >> str1;
 	ss >> str2;
-	//将veca初始化为一个二维数组,其行列值分别为str1和str2的长度加1
-	//二维数组veca记录的是两个字符串Xi和Yj的LCS长度
-	//二维数组vecb[i][j]记录veca[i][j]时所选择的子问题的最优解
-	vector<vector<int> > veca(str1.length() + 1, vector<int>(str2.length() + 1));
-	vector<vector<int> > vecb(str1.length() + 1, vector<int>(str2.length() + 1));
+	vector< vector<int> > veca(str1.length() + 1, vector<int>(str2.length() + 1));
+	vector< vector<int> > vecb(str1.length() + 1, vector<int>(str2.length() + 1));
 	cout << LCS_length(str1, str2, veca, vecb) << endl;
 	PrintOneLCS(vecb, str1, str1.length(), str2.length());
 	return 0;
